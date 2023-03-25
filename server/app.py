dd1836c6bd240270b185fac324a7d6afa7d25189
Duane <duanerichard47@hotmail.com>
2023-03-25 00:37:23 -0400

    using phase 4 project

--- server/app.py
+++ server/app.py
@@ -1,8 +1,81 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db, Article
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return ''
+
+@app.route('/get', methods=['GET', 'POST'])
+def Article():
+    if request.method == 'GET':
+        articles = Article.query.all()
+        articles_dict = [article.to_dict() for article in articles]
+        return make_response(jsonify(articles_dict), 200)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_article = Article(
+            title = data.get('title'),
+            body = data.get('body')
+        )
+        db.session.add(new_article)
+        db.session.commit()
+
+        return make_response(jsonify(new_article.to_dict()), 201)
+        
+
+
+
+@app.route('/get/<int:id>', methods= ['GET', 'DELETE', 'PATCH', 'PUT'])
+def article_by_id(id):
+    article = Article.query.filter(Article.id == id).first()
+
+    if not article:
+        return make_response(jsonify({'error': 'Article not found'}))
+
+    if request.method == 'GET':
+        return make_response(jsonify(article.to_dict()))
+    
+    elif request.method == 'DELETE':
+        db.session.delete(article)
+        db.session.commit()
+        return make_response(jsonify({'success': 'delete success'}))
+    
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(article, attr, data[attr])
+        db.session.add(article)
+        db.session.commit()
+        return make_response(jsonify(article.to_dict()), 200)
+    
+    elif request.method == 'PUT':
+        data = request.get_json()
+        
+        title = data.get('title'),
+        body = data.get('body')
+
+        article.title = title
+        article.body = body
+        
+        db.session.add(article)
+        db.session.commit()
+
+        return make_response(jsonify(article.to_dict()), 201)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,8 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
+class Article(db.Model):
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text())
+    created_at = db.Column(db.DateTime,server_default=db.func.now() )
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
